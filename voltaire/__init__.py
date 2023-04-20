bf61c52e29bbcedad412d466640f5175bf64555e
Rifltz <rifutsu@gmail.com>
2023-04-20 14:13:02 -0400

    Joined to cloud server

--- voltaire/__init__.py
+++ voltaire/__init__.py
@@ -14,8 +14,19 @@ import google.auth.transport.requests
 #to allow http traffic for local dev
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-#instantiates reference to mongdb server (the server contains the databases)
-client = MongoClient()
+with open("voltaire\database.json","r") as f:
+    #instantiates reference to mongdb server (the server contains the databases)
+    j = json.load(f)
+    client = MongoClient(j["connection"])
+
+sdb = client.students
+sidb = sdb.info
+spdb = sdb.progress
+
+tdb = client.teachers
+tidb = tdb.info
+
+cdb = client.classes
 
 GOOGLE_CLIENT_ID = "158531260771-q7vopkn3fu0l3gk6ar5s6vn9mr4s3aa1.apps.googleusercontent.com"
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
@@ -46,6 +57,7 @@ def create_app(test_config = None):
             SECRET_KEY = j["client_secret"],
             #DATABASE = os.path.join(app.instance_path, 'voltaire.sqlite'),
         )
+    
     print(GOOGLE_CLIENT_ID,app.secret_key) #HEYYY DON'T FORGET ABOUT THIS! -------------------------------------------------------------------
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -62,7 +74,7 @@ def create_app(test_config = None):
 
     def login_is_required(function):
         def wrapper():
-            if "google_id" not in session:
+            if "_id" not in session:
                 return abort(401)  # Authorization required
             else:
                 return function(g.type)
@@ -71,17 +83,20 @@ def create_app(test_config = None):
     @app.before_request
     def load_user():
         #Primarily used for loading a page with specific characteristics
-        g.user = session.get("name") #.get() to allow for None return
+        g.lang = session.get("lang") #temporarily unchangeable during development
+        g.user = session.get("name")
         g.type = session.get("type")
 
     @app.route("/login")
     def login():
+        print("login")
         authorization_url, state = flow.authorization_url()
         session["state"] = state
         return redirect(authorization_url)
     
     @app.route("/callback")
     def callback():
+        print("callback")
         flow.fetch_token(authorization_response = request.url)
         #if not session["state"] == request.args["state"]: #the problem is that session is not being saved globally
         #    abort(500)  # State does not match!
@@ -98,15 +113,33 @@ def create_app(test_config = None):
             clock_skew_in_seconds=2
         )
 
-        print(id_info)
-        session["google_id"] = id_info.get("sub")
-        session["name"] = id_info.get("name")
-        domain = id_info.get("hd")
+        print("if")
+        #Checks if the login is a new user
+        if sidb.find_one({"_id": id_info.get("sub")}) is None and tidb.find_one({"_id": id_info.get("sub")}) is None:
+            new_user = {
+                "_id": id_info.get("sub"),
+                "give_name": id_info.get("given_name"),
+                "family_name": id_info.get("family_name"),
+                "grade": None,
+                "class": ""
+            }
 
-        if domain == "ocdsb.ca":
+            #New teachers are added elsewhere
+            sidb.insert_one(new_user)
+
+        print("pas if")
+        #Fetches the
+        if (entry := sidb.find_one({"_id": id_info.get("sub")})) is not None:
+            print(entry)
             session["type"] = "student"
-        else:
+            for key in entry:
+                session[key] = entry[key]
+        elif (entry := tidb.find_one({"_id": id_info.get("sub")})) is not None:
             session["type"] = "teacher"
+            for key in entry:
+                session[key] = entry[key]
+
+        print(session)
 
         return redirect("/account")
 
@@ -122,8 +155,6 @@ def create_app(test_config = None):
             return redirect("/t/")
         elif user_type == "student":
             return redirect("/s/")
-        
-        return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
 
     from voltaire import home, student, teacher
 
