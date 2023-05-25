1467ae31fb700f427818eaa75192898d6e9fcd67
Rifltz <rifutsu@gmail.com>
2023-05-25 11:46:26 -0400

    Cleaned up import

--- voltaire/student.py
+++ voltaire/student.py
@@ -1,10 +1,8 @@
 import functools
 
 from flask import Blueprint, abort, flash, g, redirect, render_template, request, session, url_for
-
+from voltaire import db
 #from werkzeug.security import check_password_hash, generatore_password_hash
-
-#from flaskr.db import get_db
 
 #__name__ passes name of this file, auth.py
 bp = Blueprint("student", __name__, url_prefix = "/s")
--- voltaire/teacher.py
+++ voltaire/teacher.py
@@ -5,8 +5,6 @@ from voltaire import db
 
 #from werkzeug.security import check_password_hash, generatore_password_hash
 
-#from flaskr.db import get_db
-
 #__name__ passes name of this file, auth.py
 bp = Blueprint("teacher", __name__, url_prefix = "/t")
 
